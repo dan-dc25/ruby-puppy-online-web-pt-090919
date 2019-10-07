c1d92f072b953b100d8e96cb4594ec77a5b4a612
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-10-07 23:20:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,5 @@
+binding.pry
+
 class Dog
   attr_accessor :name
   @@all = [] #it is the storage container for each instance of
@@ -12,8 +14,9 @@ class Dog
   end
 
   def self.print_all
-    @@all.each do |name|
-       puts Dog.name
+    @@all.each do |obj|
+      binding.pry
+       puts 
      end
   end
 
