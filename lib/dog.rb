259dbc7aa90ff34a7322f70019bcc71f4a284c25
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-10-07 22:22:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,16 @@
 class Dog
   attr_accessor :name
-  @@all = [] #it is the storage container for each instance of 
+  @@all = [] #it is the storage container for each instance of
               #the Dog class
-  def initialize(name)            
+  def initialize(name)
     @name = name
     @@all << self
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.print_all
     @@all[0].each do |names|
       puts @name
